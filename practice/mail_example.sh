f97444b4b198edb2fc2ec784f7538d99559182f8
AMARNATH ANNA <amaranna@in.ibm.com>
2019-04-25 14:58:42 +0530

    Testing the Git commit

--- practice/mail_example.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-Recipient="amaranna@in.ibm.com"
-Subject="Script examples1"
-Message="The attached zip file has script examples"
-#`mailx -s $Subject $Recipient <<< $Message`
-mailx -s $Subject $Recipient <<< $Message
-echo "Mail subjected: $Subject sent to $Recipient"
--- practice/testmail.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-(echo To: amaranna@in.ibm.com
-  echo From: server@$HOSTNAME.com
-  echo "Content-Type: text/html; "
-  echo Subject: Test mail Subject
-  echo
-  cat email.html ) | sendmail -t
